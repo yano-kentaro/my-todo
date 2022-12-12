547a89709287869645b9dd3bcb32a3fa2e1bcae2
ken <ynkn0829@gmail.com>
2022-12-13 08:34:06 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 use axum::{routing::get, Router};
+use std::env;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-	let app = Router::new().route("/", get(root));
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
 
-	let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let app = Router::new().route("/", get(root));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-	axum::Server::bind(&addr)
-		.serve(app.into_make_service())
-		.await
-		.unwrap();
+    tracing::debug!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
 async fn root() -> &'static str {
-	"Hello, World!"
+    "Hello, World!"
 }
