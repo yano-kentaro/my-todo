32ca53b02c1fd8c03f4486ab95cbef13e0d1d949
yano <yano@interpark.co.jp>
2022-12-14 12:47:43 +0900

    テストの実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+//! # main.rs
+
 use axum::{
     http::StatusCode,
     response::IntoResponse,
@@ -55,3 +57,54 @@ struct User {
     username: String,
 }
 
+// テストの定義
+#[cfg(test)]
+mod test {
+	use super::*;
+	use axum::{
+		body::Body,
+		http::{header, Method, Request},
+	};
+	use tower::ServiceExt;
+
+	#[tokio::test]
+    async fn should_return_hello_world() {
+        let req = Request::builder()
+            .method(Method::GET)
+            .uri("/")
+            .body(Body::empty())
+            .unwrap();
+
+        let app = create_app();
+        let res = app
+            .oneshot(req)
+            .await
+            .unwrap();
+
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body = String::from_utf8(bytes.to_vec()).unwrap();
+
+        assert_eq!(body, "Hello, World!");
+    }
+
+    #[tokio::test]
+    async fn should_return_user_data() {
+        let req = Request::builder()
+            .method(Method::POST)
+            .uri("/users")
+            .header(header::CONTENT_TYPE, "application/json")
+            .body(Body::from(r#"{"username": "test"}"#))
+            .unwrap();
+
+        let app = create_app();
+        let res = app
+            .oneshot(req)
+            .await
+            .unwrap();
+
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body = String::from_utf8(bytes.to_vec()).unwrap();
+
+        assert_eq!(body, r#"{"id":1337,"username":"test"}"#);
+    }
+}
