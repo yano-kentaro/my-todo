f4d778554c40b55d034d7986ff37225e8c02fcfa
ken <ynkn0829@gmail.com>
2022-12-15 01:14:37 +0900

    HashMapでTodoデータの一時保存機能実装

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,106 @@
 //! # main.rs
 
+// ===========================================================|0
+//                        ライブラリのインポート
+// ==================================================|2022/12/15
+use anyhow::Context;
 use axum::{
+    extract::Extension,
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
     Json, Router,
 };
 use serde::{Deserialize, Serialize};
-use std::{env, net::SocketAddr};
+use std::{
+    collections::HashMap,
+    env,
+    net::SocketAddr,
+    sync::{Arc, RwLock},
+};
+use thiserror::Error;
+
+// ===========================================================|0
+//                        リポジトリエラーの定義
+// ==================================================|2022/12/15
+#[derive(Debug, Error)]
+enum RepositoryError {
+    #[error("NotFound, id is {0}")]
+    UserNotFound(i32),
+}
+
+pub trait TodoRepository: Clone + std::marker::Send + std::marker::Sync + 'static {
+    fn create(&self, payload: CreateTodo) -> Todo;
+    fn find(&self, id: i32) -> Option<Todo>;
+    fn all(&self) -> Vec<Todo>;
+    fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo>;
+    fn delete(&self, id: i32) -> anyhow::Result<()>;
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct Todo {
+    id: i32,
+    text: String,
+    completed: bool,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct CreateTodo {
+    text: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub struct UpdateTodo {
+    text: Option<String>,
+    completed: Option<bool>,
+}
+
+impl Todo {
+    pub fn new(id: i32, text: String) -> Self {
+        Self {
+            id,
+            text,
+            completed: false,
+        }
+    }
+}
+
+type TodoDatas = HashMap<i32, Todo>;
+
+#[derive(Debug, Clone)]
+pub struct TodoRepositoryForMemory {
+    store: Arc<RwLock<TodoDatas>>,
+}
+
+impl TodoRepositoryForMemory {
+    pub fn new() -> Self {
+        TodoRepositoryForMemory {
+            store: Arc::default(),
+        }
+    }
+}
+
+impl TodoRepository for TodoRepositoryForMemory {
+    fn create(&self, payload: CreateTodo) -> Todo {
+        todo!();
+    }
+
+    fn find(&self, id: i32) -> Option<Todo> {
+        todo!();
+    }
+
+    fn all(&self) -> Vec<Todo> {
+        todo!();
+    }
+
+    fn update(&self, id: i32, payload: UpdateTodo) -> anyhow::Result<Todo> {
+        todo!();
+    }
+
+    fn delete(&self, id: i32) -> anyhow::Result<()> {
+        todo!();
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -15,7 +108,8 @@ async fn main() {
     env::set_var("RUST_LOG", log_level);
     tracing_subscriber::fmt::init();
 
-    let app = create_app();
+    let repository = TodoRepositoryForMemory::new();
+    let app = create_app(repository);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
@@ -31,80 +125,63 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
-fn create_app() -> Router {
+fn create_app<T: TodoRepository>(repository: T) -> Router {
     Router::new()
         .route("/", get(root))
-        .route("/users", post(create_user))
+        .route("/todos", post(create_todo::<T>))
+        .layer(Extension(Arc::new(repository)))
 }
 
-async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
-    tracing::debug!("received request with payload: {:?}", payload);
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-    (StatusCode::CREATED, Json(user))
+pub async fn create_todo<T: TodoRepository>(
+    Json(payload): Json<CreateTodo>,
+    Extension(repository): Extension<Arc<T>>,
+) -> impl IntoResponse {
+    let todo = repository.create(payload);
+    (StatusCode::CREATED, Json(todo))
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
-struct CreateUser {
-    username: String,
-}
+// // テストの定義
+// #[cfg(test)]
+// mod test {
+//     use super::*;
+//     use axum::{
+//         body::Body,
+//         http::{header, Method, Request},
+//     };
+//     use tower::ServiceExt;
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
-struct User {
-    id: u64,
-    username: String,
-}
+//     #[tokio::test]
+//     async fn should_return_hello_world() {
+//         let req = Request::builder()
+//             .method(Method::GET)
+//             .uri("/")
+//             .body(Body::empty())
+//             .unwrap();
 
-// テストの定義
-#[cfg(test)]
-mod test {
-	use super::*;
-	use axum::{
-		body::Body,
-		http::{header, Method, Request},
-	};
-	use tower::ServiceExt;
+//         let app = create_app();
+//         let res = app.oneshot(req).await.unwrap();
 
-	#[tokio::test]
-    async fn should_return_hello_world() {
-        let req = Request::builder()
-            .method(Method::GET)
-            .uri("/")
-            .body(Body::empty())
-            .unwrap();
+//         let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+//         let body = String::from_utf8(bytes.to_vec()).unwrap();
 
-        let app = create_app();
-        let res = app
-            .oneshot(req)
-            .await
-            .unwrap();
+//         assert_eq!(body, "Hello, World!");
+//     }
 
-        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
-        let body = String::from_utf8(bytes.to_vec()).unwrap();
+//     #[tokio::test]
+//     async fn should_return_user_data() {
+//         let req = Request::builder()
+//             .method(Method::POST)
+//             .uri("/users")
+//             .header(header::CONTENT_TYPE, "application/json")
+//             .body(Body::from(r#"{"username": "test"}"#))
+//             .unwrap();
 
-        assert_eq!(body, "Hello, World!");
-    }
+//         let app = create_app();
+//         let res = app.oneshot(req).await.unwrap();
 
-    #[tokio::test]
-    async fn should_return_user_data() {
-        let req = Request::builder()
-            .method(Method::POST)
-            .uri("/users")
-            .header(header::CONTENT_TYPE, "application/json")
-            .body(Body::from(r#"{"username": "test"}"#))
-            .unwrap();
+//         let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+//         let body = String::from_utf8(bytes.to_vec()).unwrap();
 
-        let app = create_app();
-        let res = app
-            .oneshot(req)
-            .await
-            .unwrap();
-
-        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
-        let body = String::from_utf8(bytes.to_vec()).unwrap();
-
-        assert_eq!(body, r#"{"id":1337,"username":"test"}"#);
-    }
-}
+//         assert_eq!(body, r#"{"id":1337,"username":"test"}"#);
+//     }
+// }
